adc85efc806293f6419d7984873a31c7a76f53e4
Gregory <gmvitz@udel.edu>
2018-09-24 23:13:38 -0400

    Plot airport locations and color by region

--- r/network_graphs.R
+++ r/network_graphs.R
@@ -9,6 +9,8 @@ air <- import(here("data", "us_airports.csv"), setclass = "tbl_df") %>%
 
 ports <- import(here("data", "airports.csv"), setclass = "tbl_df")
 
+us <- map_data(map = "state") 
+
 airports <- left_join(air, ports, by = c("loc_id" = "V5")) %>% 
     select(
         yearly_rank:passengers,
@@ -17,19 +19,28 @@ airports <- left_join(air, ports, by = c("loc_id" = "V5")) %>%
         alt = V9) %>% 
     mutate(state = state.name[match(state, state.abb)]) %>% 
     mutate_if(
-        is.character, str_to_lower)
+        is.character, str_to_lower) %>% 
+    filter(state %in% str_to_lower(state.name),
+           state != "alaska",
+           state != "hawaii",
+           passengers != 0,
+           between(lat, min(us$lat), max(us$lat)),
+           between(long, min(us$long), max(us$long)),
+           year == 2016)
 
 
 # Begin creating the map
 
-us <- map_data(map = "state") 
-
 mapplot <- ggplot() +
     geom_map(data = airports, map = us,
-             aes(map_id = state, fill = region),
+             aes(map_id = state),
              color = "white") +
+    geom_point(data = airports, 
+               aes(x = long, y = lat, color = region, 
+                   size = passengers)) +
     expand_limits(x=us$long, y=us$lat) +
     coord_map("albers", lat0 = 39, lat1 = 45) +
+    scale_size_continuous(labels = scales::comma) +
     theme(
           panel.grid = element_blank(),
           panel.grid.minor = element_blank()
